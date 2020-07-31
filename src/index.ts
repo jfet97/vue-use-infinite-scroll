57eac5f581a4476e068a8a96c430abbbcb9527a4
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-07-31 19:46:07 +0200

    v 0.0.1

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { ref, Ref, onMounted, onUnmounted } from 'vue-demi'
+
+export type UseInfiniteScroll = (domElRef: Ref<HTMLElement>) => Ref<number>
+export type MakeUseInfiniteScroll = (options: IntersectionObserverInit) => UseInfiniteScroll
+
+export const makeUseInfiniteScroll: MakeUseInfiniteScroll = (options) => (domElRef) => {
+    const pageRef = ref(1)
+
+    let observer: IntersectionObserver | undefined = void 0
+
+    onMounted(() => {
+        observer = new IntersectionObserver(([domEl]) => {
+            if (domEl?.isIntersecting) {
+                pageRef.value++
+            }
+        }, options)
+
+        observer.observe(domElRef.value)
+    })
+
+    onUnmounted(() => {
+        observer?.disconnect()
+    })
+
+    return pageRef
+}
